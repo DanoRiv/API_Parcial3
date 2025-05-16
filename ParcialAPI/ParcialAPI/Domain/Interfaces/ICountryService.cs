6289ff6a84b2263c2ac142fe42ccdcb341e1c46b
DanoRiv <danio260496@gmail.com>
2025-05-15 21:40:42 -0500

    Add services

---
+++ ParcialAPI/ParcialAPI/Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,13 @@
+﻿using ParcialAPI.DAL.Entities;
+
+namespace ParcialAPI.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetCountriesAsync();
+        Task<Country> GetCountryByIdAsync(Guid id);
+        Task<Country> CreateCountryAsync(Country country);
+        Task<Country> UpdateCountryAsync(Country country);
+        Task<Country> DeleteCountryAsync(Guid id);
+    }
+}
