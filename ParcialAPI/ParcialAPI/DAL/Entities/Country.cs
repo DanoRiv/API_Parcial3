df6b4ae4dd09bc55f487bd1d184fe8136e720b17
DanoRiv <danio260496@gmail.com>
2025-05-15 21:38:45 -0500

    Add data access layer

---
+++ ParcialAPI/ParcialAPI/DAL/Entities/Country.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialAPI.DAL.Entities;
+
+public class Country : AuditBase
+{
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} carácteres.")]
+    [Display(Name = "País")]
+    public string Name { get; set; }
+}
