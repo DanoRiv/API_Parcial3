1b72b1288d1ed034bfaa6ccc5d5b425790691d33
DanoRiv <danio260496@gmail.com>
2025-06-04 16:12:03 -0500

    Add countries controller

---
+++ ParcialAPI/ParcialAPI/Controllers/CountriesController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParcialAPI.DAL.Entities;
+using ParcialAPI.Domain.Interfaces;
+
+namespace ParcialAPI.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class CountriesController : Controller
+{
+    private readonly ICountryService _countryService;
+
+    public CountriesController(ICountryService countryService)
+    {
+        _countryService = countryService;
+    }
+
+    [HttpGet, ActionName("Get")]
+    [Route("GetAll")]
+    public async Task<ActionResult<IEnumerable<Country>>> GetCountriesAsync()
+    {
+        var countries = await _countryService.GetCountriesAsync();
+        if (countries == null || !countries.Any())
+        {
+            return NotFound();
+        }
+        return Ok(countries);
+    }
+    [HttpGet, ActionName("Get")]
+    [Route("GetById/{id}")]
+    public async Task<ActionResult<Country>> GetCountryByIdAsync(Guid id)
+    {
+        var country = await _countryService.GetCountryByIdAsync(id);
+        if (country == null)
+        {
+            return NotFound();
+        }
+        return Ok(country);
+    }
+    [HttpPost, ActionName("Create")]
+    [Route("Create")]
+    public async Task<ActionResult<Country>> CreateCountryAsync(Country country)
+    {
+        try
+        {
+            var newCountry = await _countryService.CreateCountryAsync(country);
+            if (newCountry == null) return NotFound();
+            return Ok(newCountry);
+        }
+        catch (Exception ex)
+        {
+
+            if (ex.Message.Contains("duplicate"))
+            {
+                return Conflict(String.Format("{0} already exists", country.Name));
+            }
+            return Conflict(ex.Message);
+        }
+    }
+    [HttpPut, ActionName("Update")]
+    [Route("Update")]
+    public async Task<ActionResult<Country>> UpdateCountryAsync(Country country)
+    {
+        try
+        {
+            var updatedCountry = await _countryService.UpdateCountryAsync(country);
+            if (updatedCountry == null) return NotFound();
+            return Ok(updatedCountry);
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message.Contains("duplicate"))
+            {
+                return Conflict(String.Format("{0} already exists", country.Name));
+            }
+            return Conflict(ex.Message);
+        }
+    }
+    [HttpDelete, ActionName("Delete")]
+    [Route("Delete/{id}")]
+    public async Task<ActionResult<Country>> DeleteCountryAsync(Guid id)
+    {
+        if(id == null) return BadRequest("Country ID cannot be null.");
+        var deletedCountry = await _countryService.DeleteCountryAsync(id);
+        if (deletedCountry == null) return NotFound();
+        return Ok(deletedCountry);
+        
+    }
+}
--- ParcialAPI/ParcialAPI/Program.cs
+++ ParcialAPI/ParcialAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ParcialAPI.DAL;
+using ParcialAPI.Domain.Interfaces;
+using ParcialAPI.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddControllers();
 
 // Database connection
 builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Dependency injection for services
+builder.Services.AddScoped<ICountryService, CountryService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
